3ea616cfb9fc0542abe2d270878f176958ab5bca
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-08-03 07:02:15 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.8 MB (99.94%)

---
+++ 0074-search-a-2d-matrix/solution.java
@@ -0,0 +1,25 @@
+class Solution {
+    	public static boolean binarysearch(int []arr,int k){
+	    int low=0;
+	    int high =arr.length-1;
+	    while(low<=high){
+	        int mid=low+(high-low)/2;
+	        if(arr[mid]==k) return true;
+	        if(arr[mid]<k){
+	            low=mid+1;
+	        }else{
+	            high=mid-1;
+	        }
+	    }
+	    return false;
+	}
+    public boolean searchMatrix(int[][] arr, int k) {
+       int row=arr.length;
+       for(int i=0;i<row;i++){
+        if(binarysearch(arr[i],k)){
+            return true;
+        }
+       } 
+       return false;                                         
+    }
+}
