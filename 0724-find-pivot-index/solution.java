59b778482edbdffa4d4b3f9d6a778e34c2a53e50
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-08-05 04:08:26 +0000

    Sync LeetCode submission Runtime - 1 ms (97.38%), Memory - 45.7 MB (99.92%)

---
+++ 0724-find-pivot-index/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int pivotIndex(int[] arr) {
+        int pp=-1;
+        int n=arr.length;
+        int prefix[]=new int[n];
+        int suffix[]=new int[n];
+        prefix[0]=arr[0];
+        for(int i=1;i<n;i++){
+            prefix[i]=arr[i]+prefix[i-1];
+        }
+        suffix[n-1]=arr[n-1];
+        for(int i=n-2;i>=0;i--){
+           suffix[i]=arr[i]+suffix[i+1];
+        }for(int i=0;i<n;i++){
+            if(prefix[i]==suffix[i]) return i;
+        }
+        return pp;
+    }
+}
