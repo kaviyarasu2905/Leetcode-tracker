57959d56ef3c87019a4df6c8a44d540b3e8dbe8a
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-08-08 09:32:49 +0000

    Sync LeetCode submission Runtime - 26 ms (5.16%), Memory - 45.1 MB (15.79%)

---
+++ 0038-count-and-say/solution.java
@@ -0,0 +1,22 @@
+class Solution {
+    public String countAndSay(int n) {
+        String a = "1";
+        for (int k = 1; k < n; k++) { 
+            String s = "";
+            int i = 0;
+
+            while (i < a.length()) {
+                int cnt = 1;
+                while (i + 1 < a.length() && a.charAt(i) == a.charAt(i + 1)) {
+                    cnt++;
+                    i++;
+                }
+                s += cnt + "" + a.charAt(i);
+                i++;
+            }
+            a = s;
+        }
+        return a; 
+    }
+}
+
