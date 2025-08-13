f1081aa22ae15434308dbd9319adb0465e60b2f3
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-08-13 08:41:16 +0000

    Sync LeetCode submission Runtime - 7 ms (17.51%), Memory - 45.5 MB (99.92%)

---
+++ 0268-missing-number/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int missingNumber(int[] nums) {
+        Map<Integer,Integer> map=new HashMap<>();
+        int n=nums.length;
+        for(int i=0;i<n;i++){
+            map.put(i,1);
+        }
+        for(int i:nums){
+            map.remove(i);
+        }
+        for(int i:map.keySet()){
+            return i;
+        }return n;
+    }
+}
