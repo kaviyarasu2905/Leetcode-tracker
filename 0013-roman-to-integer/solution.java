6f88e89096d6aab7217253186cee60f5955edda4
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-08-13 04:37:07 +0000

    Sync LeetCode submission Runtime - 5 ms (31.80%), Memory - 44.6 MB (99.98%)

--- 0013-roman-to-integer/solution.java
+++ 0013-roman-to-integer/solution.java
@@ -1,31 +1,30 @@
 class Solution {
-    public int intVal(char ch){
-        if(ch=='I')return 1;
-        else if(ch=='V')return 5;
-        else if(ch=='X')return 10;
-        else if(ch=='L')return 50;
-        else if(ch=='C')return 100;
-        else if(ch=='D')return 500;
-        else if(ch=='M')return 1000;
-        else return 0;
-    }
     public int romanToInt(String s) {
+        Map<Character,Integer>map=new HashMap<>();
+        map.put('I',1);
+        map.put('V',5);
+        map.put('X',10);
+        map.put('L',50);
+        map.put('C',100);
+        map.put('D',500);
+        map.put('M',1000);
+
         int sum=0;
         int n=s.length();
         int i=0;
         for(i=0;i<s.length()-1;i++){
             char s1=s.charAt(i);
             char s2=s.charAt(i+1);
-            if(intVal(s1)<intVal(s2)){
-                sum+=intVal(s2);
-                sum-=intVal(s1);
+            if(map.get(s1)<map.get(s2)){
+                sum+=map.get(s2);
+                sum-=map.get(s1);
                 i++;
             }else{
-                sum+=intVal(s1);
+                sum+=map.get(s1);
             }
         }
         if(i!=n){
-            sum=sum+intVal(s.charAt(n-1));
+            sum=sum+map.get(s.charAt(n-1));
         }
         return sum;
     }
