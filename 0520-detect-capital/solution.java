2b904076cf035372dfc69e8b70ee5bfc59b09784
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-09-23 09:34:06 +0000

    Sync LeetCode submission Runtime - 1 ms (89.18%), Memory - 42.2 MB (99.93%)

---
+++ 0520-detect-capital/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public boolean detectCapitalUse(String word) {
+        int capitalCount = 0;
+        for (int i = 0; i < word.length(); i++) {
+            char c = word.charAt(i); 
+            if (c >= 65 && c <= 90) {
+                capitalCount++;
+            }
+        }
+        if (capitalCount == word.length()) return true;
+        if (capitalCount == 0) return true;
+        if (capitalCount == 1 && word.charAt(0) >= 65 && word.charAt(0) <= 90) return true;
+
+        return false;
+    }
+}
