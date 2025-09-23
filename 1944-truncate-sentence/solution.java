febc4f35d17e38b8f1e06f75ffc6fad0e913a7b4
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-09-23 09:43:41 +0000

    Sync LeetCode submission Runtime - 2 ms (45.42%), Memory - 42.3 MB (99.50%)

---
+++ 1944-truncate-sentence/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public String truncateSentence(String s, int k) {
+     String arr[]=s.split(" ");
+     String res[]=new String[k];
+     int index=0;
+     for(int i=0;i<k;i++){
+        res[index++]=arr[i];
+     }
+     StringBuilder sb=new StringBuilder();
+     for(int i=0;i<res.length;i++){
+        sb.append(res[i]+" ");
+     }
+     return sb.toString().trim();
+    }
+}
