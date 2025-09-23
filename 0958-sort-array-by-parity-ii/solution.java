bb377010e53a9511786317d4361d27e5517dffc8
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-09-23 06:06:33 +0000

    Sync LeetCode submission Runtime - 17 ms (5.85%), Memory - 46.8 MB (99.96%)

---
+++ 0958-sort-array-by-parity-ii/solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] sortArrayByParityII(int[] nums) {
+        int res[]=new int[nums.length];
+        List<Integer>even=new ArrayList<>();
+        List<Integer>odd=new ArrayList<>();
+        for(int i:nums){
+            if(i%2==0){
+                even.add(i);
+            }
+            else{
+                odd.add(i);
+            }
+        }
+        Collections.sort(even);
+        Collections.sort(odd);
+        for(int i=0;i<even.size();i++){
+            res[i*2]=even.get(i);
+        }
+        int index=1;
+        for(int i=0;i<odd.size();i++){
+            res[index]=odd.get(i);
+            index+=2;
+        }
+        return res;
+    }
+}
