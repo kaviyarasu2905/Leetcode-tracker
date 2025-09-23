3197741230102caa42ab047f79cc889e5af5208d
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-09-23 09:41:00 +0000

    Sync LeetCode submission Runtime - 4 ms (6.81%), Memory - 45.1 MB (98.25%)

---
+++ 1651-shuffle-string/solution.java
@@ -0,0 +1,14 @@
+class Solution {
+    public String restoreString(String s, int[] indices) {
+        int len=indices.length;
+        Map<Integer,Character>map=new HashMap<>();
+        for(int i=0;i<len;i++){
+            map.put(indices[i],s.charAt(i));
+        }
+        String res="";
+        for(int i=0;i<len;i++){
+            res+=map.get(i);
+        }
+            return res;
+    }
+}
