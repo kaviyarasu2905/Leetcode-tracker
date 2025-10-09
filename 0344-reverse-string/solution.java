569f7958fe03a3b3cdbd29f0ca5f8a6982c6079f
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-10-09 14:02:28 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 49.6 MB (5.38%)

--- 0344-reverse-string/solution.java
+++ 0344-reverse-string/solution.java
@@ -2,10 +2,10 @@ class Solution {
     public void reverseString(char[] s) {
         int i=0;
         int j=s.length-1;
-        while(i<j){
-            char temp=s[i];
+        while(i<=j){
+            char t=s[i];
             s[i]=s[j];
-            s[j]=temp;
+            s[j]=t;
             i++;
             j--;
         }
