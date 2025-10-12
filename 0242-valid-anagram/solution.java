f142cafe881c713eb92eca85fa8aa457392be88c
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-10-12 04:43:04 +0000

    Sync LeetCode submission Runtime - 4 ms (83.53%), Memory - 45.1 MB (51.40%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,13 +1,10 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        Map<Character,Integer> map1=new HashMap<>();
         if(s.length()!=t.length()) return false;
-        for(char i:s.toCharArray()){
-            map1.put(i,map1.getOrDefault(i,0)+1);
-        }
-        for(char i:t.toCharArray()){
-            map1.put(i,map1.getOrDefault(i,0)-1);
-            if(map1.get(i)<0) return false;
-        }return true;
+        char[]  a=s.toCharArray();
+        char [] b=t.toCharArray();
+        Arrays.sort(a);
+        Arrays.sort(b);
+        return Arrays.equals(a,b);
     }
 }
