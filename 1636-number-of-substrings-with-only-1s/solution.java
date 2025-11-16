f58b3b8eb64ef2cb108cb600d96789b171bbf962
Kaviyarasu G <govindharajkavi85@gmail.com>
2025-11-16 01:18:04 +0000

    Sync LeetCode submission Runtime - 6 ms (33.27%), Memory - 46.5 MB (33.63%)

---
+++ 1636-number-of-substrings-with-only-1s/solution.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSub(String s) {
+        final int mod=1000000007;
+        long cnt=0, ans=0;
+        for(char c: s.toCharArray()){
+            ans+=(1-(c-'0'))*cnt*(cnt+1)/2;
+            ans%=mod;
+            cnt=(c-'0')*(cnt+1);
+        }
+        ans=(ans+(long)cnt*(cnt+1)/2%mod);// last one
+        return (int)ans;
+    }
+}
