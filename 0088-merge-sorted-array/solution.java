bc94e0b47fa78a053a619c80008551e4690cc88b
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-05-29 08:28:28 +0000

    Sync LeetCode submission Runtime - 4 ms (17.15%), Memory - 44.1 MB (9.24%)

--- 0088-merge-sorted-array/solution.java
+++ 0088-merge-sorted-array/solution.java
@@ -1,27 +1,8 @@
 class Solution {
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-        int i = m - 1; // Last element in nums1’s original part
-        int j = n - 1; // Last element in nums2
-        int k = m + n - 1; // Last position in nums1
-
-        // Merge from the end
-        while (i >= 0 && j >= 0) {
-            if (nums1[i] > nums2[j]) {
-                nums1[k] = nums1[i];
-                i--;
-            } else {
-                nums1[k] = nums2[j];
-                j--;
-            }
-            k--;
+        for(int i=0;i<n;i++){
+            nums1[m+i]=nums2[i];
         }
-
-        // Copy remaining elements from nums2 (if any)
-        while (j >= 0) {
-            nums1[k] = nums2[j];
-            j--;
-            k--;
-        }
+        Arrays.sort(nums1);
     }
 }
-
