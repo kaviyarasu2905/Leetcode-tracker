20533147dfd57b459681be29b13d431993679a10
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-01 08:53:12 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 44.6 MB (55.82%)

--- 1791-richest-customer-wealth/solution.java
+++ 1791-richest-customer-wealth/solution.java
@@ -1,13 +1,13 @@
 class Solution {
     public int maximumWealth(int[][] accounts) {
-       int max=0; 
-       for(int i=0;i<accounts.length;i++){
-        int s=0;
-        for(int j=0;j<accounts[i].length;j++){
-            s+=accounts[i][j];
+        int max=0;
+        for(int i=0;i<accounts.length;i++){
+            int sum=0;
+            for(int j=0;j<accounts[i].length;j++){
+                sum+=accounts[i][j];
+            }
+            max=Math.max(max,sum);
         }
-        max=Math.max(max,s);
-       }
-       return max;
+        return max;
     }
 }
