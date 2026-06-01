f5a124e02ea66ebf89bf7d2c163b3e26466c9d6f
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-01 10:44:54 +0000

    Sync LeetCode submission Runtime - 5 ms (7.99%), Memory - 47.7 MB (57.59%)

--- 0283-move-zeroes/solution.java
+++ 0283-move-zeroes/solution.java
@@ -1,15 +1,22 @@
 class Solution {
     public void moveZeroes(int[] nums) {
-      int i=0;
-      for(int j=0;j<nums.length;j++){
-        if(nums[j]!=0){
-            nums[i]=nums[j];
-            i++;
+        
+        // step 1: collect all non-zero elements
+        List<Integer> nonZero = new ArrayList<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (nums[i] != 0) {
+                nonZero.add(nums[i]);
+            }
         }
-      }
-      while(i<nums.length){
-        nums[i]=0;
-        i++;
-      }
+        
+        // step 2: put non-zero elements back
+        for (int i = 0; i < nonZero.size(); i++) {
+            nums[i] = nonZero.get(i);
+        }
+        
+        // step 3: fill remaining with zeros
+        for (int i = nonZero.size(); i < nums.length; i++) {
+            nums[i] = 0;
+        }
     }
 }
