bf6472bc2848d81a008358202bcc9c4abd38651a
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-03 08:57:21 +0000

    Sync LeetCode submission Runtime - 2 ms (39.87%), Memory - 63.2 MB (35.29%)

--- 0189-rotate-array/solution.java
+++ 0189-rotate-array/solution.java
@@ -1,11 +1,12 @@
 class Solution {
     public void rotate(int[] nums, int k) {
-        k=k%nums.length;
-        int rot[]=new int[nums.length];
-        for(int i=0;i<nums.length;i++){
-            rot[(i+k)%nums.length]=nums[i];
-        }for(int i=0;i<nums.length;i++){
-            nums[i]=rot[i];
-        }
+       int r[]=new int[nums.length];
+       k=k%nums.length;
+       for(int i=0;i<nums.length;i++){
+        r[(i+k)%nums.length]=nums[i];
+       } 
+       for(int i=0;i<nums.length;i++){
+        nums[i]=r[i];
+       }
     }
 }
