f4054520efa1e4ba141888997261214180874d47
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-04 08:39:23 +0000

    Sync LeetCode submission Runtime - 2 ms (99.30%), Memory - 44.5 MB (53.93%)

--- 0125-valid-palindrome/solution.java
+++ 0125-valid-palindrome/solution.java
@@ -1,31 +1,18 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        int left = 0;
-        int right = s.length() - 1;
-
-        while (left < right) {
-
-            while (left < right &&
-                   !Character.isLetterOrDigit(s.charAt(left))) {
-                left++;
-            }
-
-            while (left < right &&
-                   !Character.isLetterOrDigit(s.charAt(right))) {
-                right--;
-            }
-
-            char l = Character.toLowerCase(s.charAt(left));
-            char r = Character.toLowerCase(s.charAt(right));
-
-            if (l != r) {
+        int i=0;
+        int j=s.length()-1;
+       while(i<j){
+        while(i<j &&!Character.isLetterOrDigit(s.charAt(i))){
+            i++;
+        }while(i<j &&!Character.isLetterOrDigit(s.charAt(j))){
+            j--;
+        }if(Character.toLowerCase(s.charAt(i)) != Character.toLowerCase(s.charAt(j))){
                 return false;
             }
-
-            left++;
-            right--;
-        }
-
-        return true;
+            i++;
+            j--;
+       }
+       return true;
     }
 }
