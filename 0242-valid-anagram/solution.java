2f52b649c4085414e96c2a8070b61ca1bed61b6e
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-04 08:43:33 +0000

    Sync LeetCode submission Runtime - 3 ms (94.05%), Memory - 46.3 MB (42.30%)

--- 0242-valid-anagram/solution.java
+++ 0242-valid-anagram/solution.java
@@ -1,10 +1,16 @@
 class Solution {
     public boolean isAnagram(String s, String t) {
-        if(s.length()!=t.length()) return false;
-        char[]  a=s.toCharArray();
-        char [] b=t.toCharArray();
-        Arrays.sort(a);
-        Arrays.sort(b);
-        return Arrays.equals(a,b);
+        if(s.length()!=t.length()){
+            return false;
+        }
+        char []a1=s.toCharArray();
+        char []a2=t.toCharArray();
+        Arrays.sort(a1);
+        Arrays.sort(a2);
+        if(Arrays.equals(a1,a2)){
+            return true;
+        }else{
+            return false;
+        }
     }
 }
