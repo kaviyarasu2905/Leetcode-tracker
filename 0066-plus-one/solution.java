f3556b2f36547873c55a1297217e0eb158f4c094
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-05 08:56:21 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 43.4 MB (43.11%)

--- 0066-plus-one/solution.java
+++ 0066-plus-one/solution.java
@@ -1,20 +1,14 @@
 class Solution {
     public int[] plusOne(int[] digits) {
-        int n = digits.length;
-
-        // Traverse from the last digit
-        for (int i = n - 1; i >= 0; i--) {
-            if (digits[i] < 9) {
-                digits[i]++; // Increment and done
-                return digits;
-            }
-            digits[i] = 0; // Carry over
+      for(int i=digits.length-1;i>=0;i--){
+        if(digits[i]<9){
+            digits[i]++;
+            return digits;
         }
-
-        // If all digits were 9, we need an extra digit at the front
-        int[] result = new int[n + 1];
-        result[0] = 1; // The rest are already 0 by default
-        return result;
+        digits[i]=0;
+      } 
+      int r[]=new int[digits.length+1];
+      r[0]=1;
+      return r; 
     }
 }
-
