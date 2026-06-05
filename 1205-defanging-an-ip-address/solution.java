92b7ad9fe9ed7f34cf75ca8e9c00fde4e82439e9
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-05 08:43:01 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.9 MB (25.37%)

--- 1205-defanging-an-ip-address/solution.java
+++ 1205-defanging-an-ip-address/solution.java
@@ -1,14 +1,13 @@
 class Solution {
     public String defangIPaddr(String address) {
-      StringBuilder sb=new StringBuilder();
-      for(int i=0;i<address.length();i++){
-        char ch=address.charAt(i);
-        if(ch=='.'){
-            sb.append("[.]");
-        }else{
-            sb.append(ch);
+        StringBuilder sb=new StringBuilder();
+        for(int i=0;i<address.length();i++){
+            if(address.charAt(i)=='.'){
+                sb.append("[.]");
+            }else{
+                sb.append(address.charAt(i));
+            }
         }
-      }  
-      return sb.toString();
+        return sb.toString();
     }
 }
