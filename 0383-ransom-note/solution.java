28521bcb41be457405497032c619eab816f6634b
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-06-10 12:41:01 +0000

    Sync LeetCode submission Runtime - 1 ms (99.95%), Memory - 46.6 MB (48.80%)

--- 0383-ransom-note/solution.java
+++ 0383-ransom-note/solution.java
@@ -1,18 +1,15 @@
 class Solution {
     public boolean canConstruct(String ransomNote, String magazine) {
-        HashMap<Character, Integer> magaHash = new HashMap<>();
+		if (ransomNote.length() > magazine.length()) return false;
+        int[] alphabets_counter = new int[26];
+        
+        for (char c : magazine.toCharArray())
+            alphabets_counter[c-'a']++;
 
-        for (char c : magazine.toCharArray()) {
-            magaHash.put(c, magaHash.getOrDefault(c, 0) + 1);
+        for (char c : ransomNote.toCharArray()){
+            if (alphabets_counter[c-'a'] == 0) return false;
+            alphabets_counter[c-'a']--;
         }
-
-        for (char c : ransomNote.toCharArray()) {
-            if (!magaHash.containsKey(c) || magaHash.get(c) <= 0) {
-                return false;
-            }
-            magaHash.put(c, magaHash.get(c) - 1);
-        }
-
         return true;
     }
 }
