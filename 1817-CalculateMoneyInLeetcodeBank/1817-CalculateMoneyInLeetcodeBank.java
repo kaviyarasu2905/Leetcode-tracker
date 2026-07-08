a8d231704571ec8fc13049db5a95cfca7645cb69
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-08 15:37:53 +0530

    Create 1817-CalculateMoneyInLeetcodeBank

---
+++ 1817-CalculateMoneyInLeetcodeBank/1817-CalculateMoneyInLeetcodeBank.java
@@ -0,0 +1,29 @@
+// Last updated: 08/07/2026, 15:37:52
+class Solution {
+    public int totalMoney(int n) {
+        int monday = 1;
+        int weeks = n/7;
+        int remainingDays = n%7;
+        int money = 0;
+
+        while(weeks-->0){
+            money+=sum(monday,7);
+            monday+=1;
+        }
+        // System.out.println(monday +" -> money : " + money);
+        while(remainingDays-->0){
+            money+=monday++;
+        }
+        return money;
+    }
+    int sum(int start, int days) {
+        int count = 0;
+        int ans = 0;
+        while(count<days) {
+            ans+=start;
+            start++;
+            count++;
+        }
+        return ans;
+    }
+}
