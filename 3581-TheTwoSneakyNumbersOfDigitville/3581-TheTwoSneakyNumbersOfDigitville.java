bd9f329d2f8bb34a5302e92730435fb1ab3400a2
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-08 15:36:48 +0530

    Create 3581-TheTwoSneakyNumbersOfDigitville

---
+++ 3581-TheTwoSneakyNumbersOfDigitville/3581-TheTwoSneakyNumbersOfDigitville.java
@@ -0,0 +1,24 @@
+// Last updated: 08/07/2026, 15:36:46
+class Solution {
+    public int[] getSneakyNumbers(int[] nums) {
+        int xor = 0;
+        int n = nums.length - 2;
+
+        for (int num : nums) xor ^= num;
+        for (int i = 0; i < n; i++) xor ^= i;
+
+        int diffBit = xor & -xor;
+
+        int a = 0, b = 0;
+        for (int num : nums) {
+            if ((num & diffBit) == 0) a ^= num;
+            else b ^= num;
+        }
+        for (int i = 0; i < n; i++) {
+            if ((i & diffBit) == 0) a ^= i;
+            else b ^= i;
+        }
+
+        return new int[]{a, b};
+    }
+}
