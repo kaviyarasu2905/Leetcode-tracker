13adb63213be96531a877b191990272af4e2ea9b
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-08 15:40:19 +0530

    Create 274-HIndex

---
+++ 274-HIndex/274-HIndex.java
@@ -0,0 +1,20 @@
+// Last updated: 08/07/2026, 15:40:18
+class Solution {
+    public int hIndex(int[] citations) {
+        int papers = citations.length;
+        int[] citationBuckets = new int[papers + 1];
+
+        for (int citation : citations) {
+            citationBuckets[Math.min(citation, papers)]++;
+        }
+
+        int cumulativePapers = 0;
+        for (int hIndex = papers; hIndex >= 0; hIndex--) {
+            cumulativePapers += citationBuckets[hIndex];
+            if (cumulativePapers >= hIndex) {
+                return hIndex;
+            }
+        }
+        return 0;        
+    }
+}
