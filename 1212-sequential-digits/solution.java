0cd14e7f3e8faa8e8c8b544716ea9f5b4b014673
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-14 06:04:53 +0000

    Sync LeetCode submission Runtime - 0 ms (100.00%), Memory - 42.3 MB (67.37%)

---
+++ 1212-sequential-digits/solution.java
@@ -0,0 +1,19 @@
+class Solution {
+    public List<Integer> sequentialDigits(int low, int high) {
+        List<Integer> lst = new ArrayList<>();
+        for(int i=1;i<=9;i++)
+        {
+            int num=i;
+            for(int j=i+1;j<=9;j++)
+            {
+                num=num*10+j;
+                if(num>=low && num<=high)
+                {
+                    lst.add(num);
+                }
+            }
+        }
+        Collections.sort(lst);
+        return lst;
+    }
+}
