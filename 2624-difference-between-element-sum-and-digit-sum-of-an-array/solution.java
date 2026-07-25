2396907978918495bca39c21a370b22ce0e917bd
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-25 08:54:25 +0000

    Sync LeetCode submission Runtime - 2 ms (98.57%), Memory - 45.9 MB (97.99%)

---
+++ 2624-difference-between-element-sum-and-digit-sum-of-an-array/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int differenceOfSum(int[] nums) {
+      int e=0;
+      int d=0;
+      for(int i=0;i<nums.length;i++){
+        e+=nums[i];
+        int temp=nums[i];
+        while(temp!=0){
+            d+=temp%10;
+            temp/=10;
+        }
+      }  
+      return Math.abs(e-d);
+    }
+}
