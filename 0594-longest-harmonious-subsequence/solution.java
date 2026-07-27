67e6ab4eb64a806c9d1151ac54a8039a715162a5
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-07-27 06:14:54 +0000

    Sync LeetCode submission Runtime - 17 ms (40.36%), Memory - 48.1 MB (70.92%)

---
+++ 0594-longest-harmonious-subsequence/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int findLHS(int[] nums) {
+        Arrays.sort(nums);
+        int j=0;
+        int m=0;
+        for(int i=0;i<nums.length;i++){
+            while(nums[i]-nums[j]>1){
+                j++;
+            }
+            if(nums[i]-nums[j]==1){
+                m=Math.max(m,i-j+1);
+            }
+        }
+        return m;
+    }
+}
