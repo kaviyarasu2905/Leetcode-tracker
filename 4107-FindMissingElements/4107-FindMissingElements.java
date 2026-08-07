0decd71e9f5176c548be6fb267be7ba5e2958ff9
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-08-07 09:55:47 +0530

    Create 4107-FindMissingElements

---
+++ 4107-FindMissingElements/4107-FindMissingElements.java
@@ -0,0 +1,19 @@
+// Last updated: 07/08/2026, 09:55:59
+class Solution {
+    public List<Integer> findMissingElements(int[] nums) {
+        List<Integer> res = new ArrayList<>();
+
+        Arrays.sort(nums);
+
+        int currEle = nums[0];
+
+        for(int i = 0 ; i < nums.length ; currEle++, i++){
+            if(currEle < nums[i]){
+                res.add(currEle);
+                i--;
+            }
+        }
+
+        return res;
+    }
+}
