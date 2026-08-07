9f567684a43baa590cc7643d985c521360c7ff27
Kaviyarasu G <govindharajkavi85@gmail.com>
2026-08-07 10:03:02 +0530

    Create 495-TeemoAttacking

---
+++ 495-TeemoAttacking/495-TeemoAttacking.java
@@ -0,0 +1,15 @@
+// Last updated: 07/08/2026, 10:03:14
+class Solution {
+    public int findPoisonedDuration(int[] t, int d) {
+        int sum=0;
+        for(int i=0;i<t.length-1;i++){
+            if(t[i+1]<=t[i]+d-1){
+                sum+=t[i+1]-t[i];
+            }else{
+                sum+=d;
+            }
+        }
+        sum+=d;
+        return sum;
+    }
+}
